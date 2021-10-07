52c290e05d79404a5df3ffde05e649d7f6133a68
unknown <jahinhasan48@gmail.com>
2021-10-07 13:10:25 +0600

    jisan

---
+++ explore.js
@@ -0,0 +1,16 @@
+const student = {
+    name: 'joll',
+    friend: 'borsha',
+    job: 'Business',
+    makeMovie: function (names) {
+        console.log(names);
+    }
+}
+
+student.makeMovie('jisan')
+
+
+
+// Web Api / browser Api 
+/* document.getElementById()
+fetch  */
